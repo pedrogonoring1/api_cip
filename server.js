7f724479bc6805a3cb6ae0368e78d1ea178e0dc8
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2020-07-25 12:24:21 -0300

    Deixando publica a conexao

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const connectDB = require('./data/Connection');
 const bodyParser = require('body-parser');
 
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
 const app = express();
+
+connectDB()
+const Port = process.env.Port || 8877;
+
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 
-connectDB()
-const Port = process.env.Port || 8877;
 
 //Chamando as rotas
 app.use('/user', require('./routers/user'));
