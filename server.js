45597826bb7166e043314cec3e0d96e03a581d46
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2020-07-24 23:05:35 -0300

    Project initial

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./data/Connection');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+connectDB()
+const Port = process.env.Port || 8877;
+
+//Chamando as rotas
+app.use('/user', require('./routers/user'));
+app.use('/car', require('./routers/car'));
+app.use('/locacao', require('./routers/locacao'));
+
+
+//app.listen(Port, () => console.log('Servidor Iniciado!'));
+app.listen(process.env.PORT || 8080);
+
+
+module.exports = app;
